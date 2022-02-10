37e05cd7d1a272ddd49a203f0eb16aff26eba0fd
edwardgurney <edwardgurney1979@gmail.com>
2022-02-10 19:09:48 +0000

    Added allauth templates and base templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+# if os.path.isfile('env.py'):
+#     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(e2s%bm9-o2(qsfs14u29$ys+s(!qn*rhp2b4-nwe6ydjg0(g2'
-
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
