a12a6da3157668801c3e0c4164be99f240b479cc
edwardgurney <edwardgurney1979@gmail.com>
2022-02-18 18:30:05 +0000

    added sorting and back to top link

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q 
+from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
+
 
 # Create your views here.
 
@@ -21,7 +24,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category_name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
