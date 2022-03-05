38eba57a9182d78ba2247917b0635a00c44f1410
edwardgurney <edwardgurney1979@gmail.com>
2022-03-05 10:11:56 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KZtgpJwHebs8RemkP8kdiwAGFlvGXFGLbORvIsysaGHbH6wM5T8b4UB8syEcozRMqVdXvVwDD3VRea3tIqnvik200JjUOXykO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
